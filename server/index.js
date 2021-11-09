0b6788eb69f9c1cfc9f457b725c91000a4061692
David Wallace <dwallacemarketing@gmail.com>
2021-11-08 22:16:02 -0600

    crud updates

--- server/index.js
+++ server/index.js
@@ -12,44 +12,23 @@ app.use(express.json());
 
 //routes
 
-app.post("/test/todos", (req, res) => {
-    console.log(req.body)
 
-    res.status(201).json({
-        status: "sucess",
-    })
-})
 
-app.get("/test/todos/:id", (req, res) => {
-    console.log(req.params)
 
-    res.json({
-        status: "success",
-    })
-
-})
-
-app.post("/todos", async (req, res) => {
+app.post("/api/tasks", async (req, res) => {
     try {
         const { description } = req.body;
-        console.log(description)
 
-
-        const newTodo = await database.query(
+        const newTask = await database.query(
             "INSERT INTO todo (description) VALUES($1) RETURNING *",
             [description]
         );
-        console.log(newTodo)
-
-
         res.status(201).json({
             status: "Success",
             data: {
-                input: newTodo.rows[0],
-                "null": "null"
+                input: newTask.rows[0]
             }
         });
-        // res.json(newTodo.rows[0]);
 
     } catch (err) {
         console.error(err.message);
@@ -58,17 +37,57 @@ app.post("/todos", async (req, res) => {
 
 //get all todos
 
-app.get("/todos", async (req, res) => {
+app.get("/api/tasks", async (req, res) => {
     try {
-        const allTodos = await database.query("SELECT * FROM todo");
-        res.json(allTodos.rows);
+        const allTasks = await database.query("SELECT * FROM todo");
+        res.status(201).json({
+            data: allTasks.rows
+        });
+
     } catch (err) {
         console.error(err.message);
     }
 });
 
+//get a specific task
+app.get("/api/tasks/:id", async (req, res) => {
+    console.log(req.params)
+
+    task_id = req.params.id;
+
+    try {
+        const thisTask = await database.query("SELECT * FROM todo WHERE todo_id = $1", [task_id]);
+        res.status(201).json({
+            data: thisTask.rows[0].description
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+
+})
+
 //edit todo
 
+app.put("/api/tasks/:id", async (req, res) => {
+    console.log(req.params)
+
+    task_id = req.params.id;
+    task_description = req.body.description
+
+    console.log(task_description)
+    try {
+        const this_task = await database.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [task_description, task_id]
+        );
+
+        res.status(201).json({
+            data: `Task # ${task_id}, was updated to ${task_description}`
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+
+})
 //delete todo
 
 
