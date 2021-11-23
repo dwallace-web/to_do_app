eb1dbf701d068ac8e34853573b83051325fd3cda
David Wallace <dwallacemarketing@gmail.com>
2021-11-22 22:16:30 -0600

    encrpytion  updates

--- server/routes/authenticate.js
+++ server/routes/authenticate.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 // const router = express.Router()
 const database = require("../database");
-
+const bycrypt = require("bcrypt");
 const JWT = require("../util/security");
-
-
 
 //sign up
 
@@ -25,9 +23,16 @@ router.post('/signup', async (req, res) => {
             res.status(400).send('this user exists already. login instead')
         } else {
             console.log([user_detail_email, user_detail_pw])
-            console.log('test')
+
+            let saltRounds = 12;
+            let salt = await bycrypt.genSalt(saltRounds)
+            let brcryptPassword = await bycrypt.hash(user_detail_pw, salt)
+
+
             let newUser = await database.query("INSERT INTO users (user_email, user_pw) VALUES($1, $2) RETURNING *",
-                [user_detail_email, user_detail_pw]);
+                [user_detail_email, brcryptPassword]);
+
+            const localToken = JWT(newUser.rows[0].user_id)
 
             res.status(201).json({
                 status: "Success",
@@ -35,7 +40,8 @@ router.post('/signup', async (req, res) => {
                     input: {
                         email: newUser.rows[0].user_email,
                         id: newUser.rows[0].user_id
-                    }
+                    },
+                    token: localToken
                 }
             });
         }
